7cbe08d1a322155eb6954dd0eeb68ef820ad62b5
Phaniram <phaniramkuppala@gmail.com>
2022-04-22 11:08:52 +0530

    First Commit

---
+++ Ifdemo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Ifdemo {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		a= sc.nextInt();
+		b= sc.nextInt();
+		System.out.println("Enter the no.1="+a);
+		System.out.println("Enter the no.2="+b);
+		System.out.println("Sum of the two no's is"+(a+b));
+		System.out.println("Sub of the two no's is"+(a-b));
+		System.out.println("multiplication of the two no's is"+(a*b));
+		System.out.println("division of the two no's is"+(a/b));
+		System.out.println("modulo of the two no's is"+(a%b));
+		
+		
+
+	}
+
+}
